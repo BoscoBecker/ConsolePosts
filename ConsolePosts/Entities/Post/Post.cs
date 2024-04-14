136d1df9fa91ebf7301051cc980173f8ce96d6a4
Joao Bosco Dos Reis Becker <joao.jbosco@gmail.com>
2024-04-14 17:33:08 -0300

    Add project files.

---
+++ ConsolePosts/Entities/Post/Post.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsolePosts.Entities.Comments;
+
+namespace ConsolePosts.Entities.Post
+{
+    class Post
+    {
+        public DateTime Moment { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int likes { get; set; }
+
+        private List<Comment> Comments { get; set; } = new List<Comment>();
+
+        public Post(){
+            
+        }
+
+        public Post(DateTime moment, string title, string content, int likes)
+        {
+            Moment = moment;
+            Title = title;
+            Content = content;
+            this.likes = likes;
+        }
+
+        public void AddComment(Comment comment) {
+            Comments.Add(comment);
+        }
+
+        public void RemoveComment(Comment comment)
+        {
+            Comments.Remove(comment);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sbPosts = new StringBuilder();
+            sbPosts.AppendLine(Title);
+            sbPosts.Append(likes);
+            sbPosts.AppendLine(" Likes - ");
+            sbPosts.AppendLine(Moment.ToString("dd/mm/yyy HH:mm:ss"));
+            sbPosts.AppendLine(Content);
+            foreach (Comment comment in Comments) {
+                sbPosts.AppendLine(comment.Text);
+            }
+            return sbPosts.ToString();
+
+        }
+    }
+}
---
+++ ConsolePosts/Program.cs
@@ -0,0 +1,241 @@
+﻿using ConsolePosts.Entities.Comments;
+using ConsolePosts.Entities.Post;
+
+namespace ConsolePosts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Comment comment1 = new Comment("Have a Nice tripe");
+            Comment comment2 = new Comment("How That's Awesome");
+
+            Post post1 = new Post(
+                DateTime.Parse("21/06/2018 12:00:00"),
+                "Travelling to new Zealand",
+                "I'm going to visit this wonderful contry",
+                12);
+
+            post1.AddComment(comment1);
+
+            Post post2 = new Post(
+                DateTime.Parse("01/06/2020 19:00:01"),
+                "Traveling to new USA",
+                "I'm going to visit this contry, American Dream",
+                50);
+
+            post2.AddComment(comment2);
+
+
+            Console.WriteLine(post1.ToString());
+            Console.WriteLine(post2.ToString());
+
+
+        }
+    } 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
